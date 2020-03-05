a57ae141acb38cfa2bb6403fd240dcb774fe8a2b
mountainstar <chriswirthdesign@gmail.com>
2020-03-04 21:50:07 -0700

    end of section 3

---
+++ client-app/src/App.tsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Header, Icon, List } from "semantic-ui-react";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    values: []
+  };
+
+  async componentDidMount() {
+    const { data } = await axios.get("http://localhost:5000/api/values");
+
+    this.setState({
+      values: data
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header as="h2">
+          <Icon name="users" />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
+      </div>
+    );
+  }
+}
+
+export default App;
